91e9e6c2c66505de60f0c431e7f10f3b00d6c280
Jørgen Bye <joergen.bye@gmail.com>
2024-12-08 18:57:15 +0100

    8 part 1

---
+++ 08/main.js
@@ -0,0 +1,82 @@
+console.time("elapsed");
+import { readFileSync, writeFileSync } from "node:fs";
+const data = readFileSync("input.txt", "utf8");
+const rows = data.split("\n");
+rows.pop();
+
+const maxY = rows.length;
+const maxX = rows[0].length;
+
+const regex = /[a-zA-Z0-9]/g;
+
+const antennas = new Map();
+
+for (let i = 0; i < rows.length; i++) {
+  const matches = rows[i].match(regex);
+  if (matches) {
+    matches.forEach((match) => {
+      const current = antennas.get(match);
+      const ny = { y: i, x: rows[i].indexOf(match) };
+      antennas.set(match, current ? [...current, ny] : [ny]);
+    });
+  }
+}
+
+const antinodes = [];
+
+antennas.forEach((antenna) => {
+  for (let j = 0; j < antenna.length; j++) {
+    const rest = [...antenna];
+    rest.splice(0, j + 1);
+    rest.forEach((position) => {
+      let xDiff = Math.abs(position.x - antenna[j].x);
+      let yDiff = Math.abs(position.y - antenna[j].y);
+
+      try {
+        const nodeBefore = {
+          y: antenna[j].y - yDiff,
+          x:
+            antenna[j].x < position.x
+              ? antenna[j].x - xDiff
+              : antenna[j].x + xDiff,
+        };
+
+        const nodeAfter = {
+          y: position.y + yDiff,
+          x:
+            antenna[j].x > position.x ? position.x - xDiff : position.x + xDiff,
+        };
+
+        if (isInMap(nodeBefore)) {
+          antinodes.push(`${nodeBefore.x}:${nodeBefore.y}`);
+        }
+
+        if (isInMap(nodeAfter)) {
+          antinodes.push(`${nodeAfter.x}:${nodeAfter.y}`);
+        }
+      } catch {
+        console.log("whaaa");
+      }
+    });
+  }
+});
+
+function setCharAt(str, index, chr) {
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
+}
+function isInMap({ x, y }) {
+  return x >= 0 && x < maxX && y >= 0 && y < maxY;
+}
+const nodesWithinBounds = new Set(antinodes);
+
+Array.from(nodesWithinBounds).map((n) => {
+  const [x, y] = n.split(":").map((c) => Number(c));
+  rows[y] = setCharAt(rows[y], x, "#");
+});
+
+writeFileSync("output.txt", rows.join("\n"));
+
+console.log("total:", nodesWithinBounds.size);
+
+console.timeEnd("elapsed");
